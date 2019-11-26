25f9a99f9f1409c7e0d7b71665efcbc6f4ff280d
Kien Nguyen <kienkun1990@gmail.com>
2019-11-26 11:57:12 +0700

    update 26/11

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -1,10 +1,13 @@
 package com.codegym.controller;
 
+import com.codegym.model.Customer;
 import com.codegym.service.CustomerService;
 import com.codegym.service.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
@@ -12,8 +15,10 @@ public class CustomerController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("customers", customerService.findAll());
-        return "index";
+
+        List<Customer> customerList = customerService.findAll();
+        model.addAttribute("customers", customerList);
+        return "/index";
     }
 
 }
